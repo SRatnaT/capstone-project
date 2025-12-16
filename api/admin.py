b1dc98c2efd3c8eb049109864e9b9511d0749e04
SRatnaT <sailratna1@gmail.com>
2025-12-16 14:25:40 +0545

    Day 8 complete: django admin portal set up and optimized

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,33 @@
+# flake8:noqa
+
 from django.contrib import admin
 
 from .models import Comment, Project, Task
 
 # Register your models here.
 
-admin.site.register(Project)
-admin.site.register(Task)
-admin.site.register(Comment)
+
+class ProjectAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "name", "description", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("name", "description")
+
+
+class TaskAdmin(admin.ModelAdmin):
+
+    list_display = ("title", "description", "is_completed", "created_at", "project")
+    list_filter = ("created_at", "is_completed")
+    search_fields = ("title", "description", "project")
+
+
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ("content", "created_at", "task")
+    list_filter = ("content", "created_at")
+    search_fields = ("content", "task")
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Comment, CommentAdmin)
