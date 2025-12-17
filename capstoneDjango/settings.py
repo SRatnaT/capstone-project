1e33bd4200a1b1d65d831fca32b5da6718fcd53e
SRatnaT <sailratna1@gmail.com>
2025-12-17 13:51:06 +0545

    Day 9 complete: connection with postgresql and ran raw sql queries

--- capstoneDjango/settings.py
+++ capstoneDjango/settings.py
@@ -76,10 +76,19 @@ WSGI_APPLICATION = "capstoneDjango.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "capstone_db",
+        "USER": "postgres",
+        "PASSWORD": "Arsenal4444!",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
